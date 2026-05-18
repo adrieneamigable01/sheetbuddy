576f29f8be3d3fd4a4a4e4e4a9cd12d286a2f620
Adriene Amigable <adrienecarrreamigable01@gmail.com>
2026-05-18 09:22:11 +0800

    added config example

---
+++ assets/js/config-example.js
@@ -0,0 +1,11 @@
+
+/* --- GOOGLE PICKER CONFIG --- */
+const CLIENT_ID = 'Example Client ID';
+const API_KEY = 'Example API Key';
+const SCOPES = "https://www.googleapis.com/auth/drive.metadata.readonly " + 
+               "https://www.googleapis.com/auth/drive.readonly " + 
+               "https://www.googleapis.com/auth/spreadsheets.readonly " + 
+               "https://www.googleapis.com/auth/userinfo.profile " + 
+               "https://www.googleapis.com/auth/userinfo.email";
+
+// Note create a config.js on the file then copy paste this code
